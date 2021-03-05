4299de3880d588adc73a8f1b9c2d3be23299f6da
tsuzuki-auk <wimps.r0930@icloud.com>
2021-03-05 19:19:47 +0900

    ルーティングの設定（登録画面表示/ToDo登録）

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,9 @@ use Illuminate\Support\Facades\Route;
 
 // 一覧画面を表示
 Route::get('/', 'App\Http\Controllers\PostController@showList')->name('posts');
+
+// 登録画面を表示
+Route::get('/post/create', 'App\Http\Controllers\PostController@showCreate')->name('create');
+
+// ToDoの登録
+Route::post('/post/register', 'App\Http\Controllers\PostController@exeRegister')->name('register');
