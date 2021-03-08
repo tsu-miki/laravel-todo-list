ff9f70e7af25e39c2972c1ba4ef147e769efeae7
tsuzuki-auk <wimps.r0930@icloud.com>
2021-03-08 12:35:28 +0900

    削除ボタンの作成

--- resources/views/post/list.blade.php
+++ resources/views/post/list.blade.php
@@ -10,6 +10,7 @@
         <th>内容</th>
         <th>登録時間</th>
         <th></th>
+        <th></th>
       </tr>
       @foreach($posts as $post)
       <tr>
@@ -17,9 +18,22 @@
         <td>{{ $post->content }}</td>
         <td>{{ $post->created_at }}</td>
         <td><button type="button" class="btn btn-primary" onclick="location.href='/post/edit/{{ $post->id }}'">編集</button></td>
+        <form method="POST" action="{{ route('delete', $post->id) }}" onSubmit="return checkDelete()">
+        @csrf
+        <td><button type="submit" class="btn btn-danger">削除</button></td>
+        </form>
       </tr>
       @endforeach
     </table>
   </div>
 </div>
+<script>
+function checkDelete(){
+if(window.confirm('削除してよろしいですか？')){
+    return true;
+} else {
+    return false;
+}
+}
+</script>
 @endsection
