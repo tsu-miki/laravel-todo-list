c54eaae18e6ffc78177f3d4a99c6a0865bf70ae5
tsuzuki-auk <wimps.r0930@icloud.com>
2021-03-08 12:11:04 +0900

    コントローラ作成（編集画面表示/編集機能）

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Models\Post;
 use App\Http\Requests\PostRequest;
+use PhpParser\Node\Expr\FuncCall;
 
 class PostController extends Controller
 {
@@ -28,7 +29,7 @@ class PostController extends Controller
     }
 
     /**
-     * ToDo登録
+     * ToDo登録機能
      * 
      * @return view
      */
@@ -37,9 +38,44 @@ class PostController extends Controller
         $inputs = $request->all();
         // ToDoを登録
         Post::create($inputs);
-
+        
         \Session::flash('err_msg', 'ToDoを登録しました。');
         return redirect(route('posts'));
     }
 
+    /**
+     * ToDo編集画面表示
+     * 
+     * @param int $id
+     * @return view
+     */
+    public function showEdit($id) {
+        $post = Post::find($id);
+
+        if(is_null($post)) {
+            \Session::flash('err_msg', 'データがありません。');
+            return redirect(route('posts'));
+        }
+
+        return view('post.edit', ['post' => $post]);
+    }
+
+    /**
+     * ToDo編集機能
+     * 
+     * @return view
+     */
+    public function exeUpdate(PostRequest $request) {
+        // 投稿データの受け取り
+        $inputs = $request->all();
+        // ToDoを更新
+        $post = Post::find($inputs['id']);
+        $post->fill([
+            'content' => $inputs['content']
+        ]);
+        $post->save();
+        
+        \Session::flash('err_msg', 'ToDoを更新しました。');
+        return redirect(route('posts'));
+    }
 }
