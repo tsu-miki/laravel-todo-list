d9e374fd551304cddfd809d41346d0a3af4a9daa
tsuzuki-auk <wimps.r0930@icloud.com>
2021-03-08 13:09:29 +0900

    メニューの修正

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,10 +5,8 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
-            <a class="nav-item nav-link active" href="{{ route('posts') }}">ToDo一覧 <span class="sr-only"></span></a>
-            <a class="nav-item nav-link" href="{{ route('create') }}">登録</a>
-            <a class="nav-item nav-link" href="#">削除</a>
-            <a class="nav-item nav-link" href="#">更新</a>
+            <a class="nav-item nav-link active" href="{{ route('posts') }}">一覧ページ<span class="sr-only"></span></a>
+            <a class="nav-item nav-link" href="{{ route('create') }}">ToDo登録</a>
         </div>
     </div>
 </nav>
